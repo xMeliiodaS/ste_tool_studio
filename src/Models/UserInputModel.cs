54b8f4b147ed6bdbab93cbc532ca455718da8e74
Abozalaf <babozala@its.jnj.com>
2026-03-26 08:40:28 +0200

    Delete unnecessay file

--- src/Models/UserInputModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ste_tool_studio.Models
-{
-    public class UserInputModel
-    {
-        public string ExcelFilePath { get; set; }
-        public string StdName { get; set; }
-    }
-}
